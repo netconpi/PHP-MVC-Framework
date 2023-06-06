57163383bfe963f7379ce364961f66ad1288b8bf
netconpi <mishadesignsev@yandex.ru>
2023-06-06 15:25:31 +0300

    good idea to keep all public files in certain folder for safety. But...

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+echo $_SERVER['DOCUMENT_ROOT'];
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 use \app\core\Application;
 
 $app = new Application();
@@ -8,10 +10,12 @@ $app = new Application();
 $app->router->get('/', function (): void {
     echo 'Hello world!';
 });
+
 $app->router->get('/contacts', function (): void {
     echo 'Контакты!';
 });
 
+echo 'Run called!';
 $app->run();
 
 
